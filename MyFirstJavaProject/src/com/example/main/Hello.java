7012a65188ef3896d3321d67e700b7309fe39e36
metalmike09 <mdcunni@gmail.com>
2016-04-19 14:10:45 -0500

    Initial commit from local repo

---
+++ MyFirstJavaProject/src/com/example/main/Hello.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Integrate java project with Git..");
+	
+		System.out.println("First Git Commit line");
+		
+		System.out.println("First Git Commit line");
+		System.out.println("First Git Commit line");
+		
+		System.out.println("metalmike09");
+		
+		
+	}
+}
